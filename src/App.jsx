90c20aa3a20901e98871123a05f72bcb187e4b84
Enrico <e.giacomazzi@contralto-audio.com>
2025-10-02 12:38:36 +0200

    todo list first try

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
 import style from './App.module.css';
 import { useState } from 'react';
 
+
+const initialState = [
+  {id: 1, text: 'fare la spesa', done: false},
+  {id: 2, text: 'guardare batman', done: false},
+  {id: 3, text: 'imparare a usare i colori', done: false},
+  {id: 4, text: 'banana', done: false},
+]
+
 function App() {
-  const [count, setCount] = useState(42);
+  const [todos, setTodos] = useState(initialState);
 
-  function clickHandler() {
-    setCount(x => x - 1);
+  // console.log('todos', todos);
+
+  function tmp() {
+    const a1 = [1, 2, 3];
+    const a2 = a1.map(x => x * 5);
+    console.log(a2);
   }
 
-  const h1Style = {
-    color: 'blue'
-  }
 
   return (
     <>
-      <button onClick={clickHandler}>Click me</button>
-      <h1 className={count % 2 === 0 ? 'pippo' : 'pluto'}>{count}</h1>
-      <h2 className={style.ciccio}>ciccio</h2>
-      <h2 className="yellow green"> ciaone</h2>
+    <button onClick={tmp}>ciao</button>
+      <ul>
+        {todos.map(t => <li key={t.id}>{t.text}</li>)}
+      </ul>
     </>
   )
 }
